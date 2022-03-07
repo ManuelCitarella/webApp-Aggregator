583be81c672fcbde68c6a500632df6a36017ead4
ManuelCitarella <manuel.citarella@libero.it>
2022-03-07 12:08:47 +0100

    Add: View for different attributes

--- client/src/components/ButtonClick.js
+++ client/src/components/ButtonClick.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import _ from "lodash";
 import ProjectData from '../projectData.json';
+import { TableOnDate, TableOnEmployee,  TableOnProject, TableOn_Date_Employee, TableOn_Date_Project, TableOn_Employee_Date, TableOn_Employee_Project, TableOn_Project_Date, TableOn_Project_Employee } from './TableViewer';
 
-var ProjectT = ProjectData;
+//var ProjectT = fetch('http://localhost:3001/json')
+var ProjectT = ProjectData; // Json Array taken from file 
 
 export class Aggregation extends React.Component {
     constructor() {
@@ -15,22 +17,34 @@ export class Aggregation extends React.Component {
     this.publish = this.publish.bind(this);
     this.handleChange = this.handleChange.bind(this);
 };
-    
-    Aggregation(attribute1, attribute2){
-        if(this.state.comboboxValue2=="N/A" || this.state.comboboxValue2==""){
-        return this.executeAggWithPar(attribute1);}
-        else{ 
-            return this.AggregationWith2Attributes(attribute1,attribute2);
+
+    /* Main Aggregation function that checks if there are
+       one or two values or none in the comboboxes.
+       In this way, if there is only one attribute execute 'AggregationOn1',
+       while if there are two attributes execute 'AggregationOn2'
+       @Param attribute1 -> first attribute of the table
+       @Param attribute2 -> second attribute of the table
+       @Return table -> aggregated table on one or two attributes */
+    mainAggregation(attribute1, attribute2){
+        this.publish();
+        if(this.state.comboboxValue2==="N/A" || this.state.comboboxValue2===""){ // if there is a value only in the first combobox
+        return this.aggregationOn1(attribute1);
+        }
+        else{ // if there is a value in every combobox
+            return this.aggregationOn2(attribute1,attribute2); 
         }   
     }
 
-    /* Execute Aggregation with only one attribute */
-    executeAggWithPar(text1){
-        this.publish();
-        var grouping =  _.groupBy(ProjectT, text1);
+    /* Execute Aggregation on only one attribute 
+       @Param attribute1 -> attribute of table
+       @Return table -> aggregated table on one attribute*/
+    aggregationOn1(attribute1){
 
-        var result = _.map(grouping, function(value, key) {
-            return { 
+        var grouping, result, final; // local vars
+
+        grouping =  _.groupBy(ProjectT, attribute1); // groups based on one attribute given using lodash 'groupBy'
+        result = _.map(grouping, function(value, key) { // maps the new table on all values
+        return { 
                 Project: _.reduce(value, function(total, o) { 
                     return o.project.name;
                 }, 0), 
@@ -44,74 +58,48 @@ export class Aggregation extends React.Component {
                     return total + o.hours;
                 }, 0) 
             };
-            });
+        });
         
-        if(text1=="project.name"){
-        const final = result.map((data, i) => (
+        if(attribute1==="project.name"){ // if the selected value is 'Project', displays specific values 
+        final = result.map((data, i) => ( // displays new table values on screen
                 <div key={i}>
-                    <table>
-                    <tbody>
-                    <tr>
-                    <td>
-                    Project
-                    <h5>{data.Project}</h5>
-                    </td> 
-                    <td>
-                    Hours
-                    <h5>{data.Hours}</h5>
-                    </td>
-                    </tr>
-                    </tbody>
-                    </table>
+                    <TableOnProject
+                    project = {data.Project}
+                    hours = {data.Hours}/>
                 </div>));
         return final;
-        }else if(text1=="employee.name"){
-            const final = result.map((data, i) => (
+        }else if(attribute1==="employee.name"){ // else display on 'Employee'
+            final = result.map((data, i) => (
                 <div key={i}>
-                    <table>
-                    <tbody>
-                    <tr>
-                    <td>
-                    Employee
-                    <h5>{data.Employee}</h5>
-                    </td> 
-                    <td>
-                    Hours
-                    <h5>{data.Hours}</h5>
-                    </td>
-                    </tr>
-                    </tbody>
-                    </table>
+                    <TableOnEmployee
+                    employee={data.Employee}
+                    hours={data.Hours}/>
                 </div>));
         return final;
-        }else if(text1=="date"){
-            const final = result.map((data, i) => (
+        }else if(attribute1==="date"){ // else on 'Date'
+            final = result.map((data, i) => (
                 <div key={i}>
-                    <table>
-                    <tbody>
-                    <tr>
-                    <td>
-                    Date
-                    <h5>{data.Date}</h5>
-                    </td> 
-                    <td>
-                    Hours
-                    <h5>{data.Hours}</h5>
-                    </td>
-                    </tr>
-                    </tbody>
-                    </table>
+                    <TableOnDate
+                    date = {data.Date}
+                    hours = {data.Hours}/>
                 </div>));
         return final;
         }
     }
 
-    /* Execute Aggregation with two attributes */
-    AggregationWith2Attributes(text1,text2){
-        if(text1 == "project.name" && text2 == "employee.name"){
-        const grouped = _.groupBy(ProjectT, item => `"${item.project.id}+${item.employee.id}"`);
-        // RAGGRUPPAMENTO MULTIPLO PROJECT.ID E EMPLOYEE.ID
-        var result3 = _.map(grouped, function(value, key) {
+    /* Execute Aggregation on two attributes 
+       @Param attribute1 -> first attribute of the table
+       @Param attribute2 -> second attribute of the table
+       @Return table -> aggregated table on two attributes*/
+    aggregationOn2(attribute1,attribute2){
+
+        var grouped, newTable, sorted, final; // local vars
+
+        if(attribute1 === "project.name" && attribute2 === "employee.name"){ // if attributes have these values
+        
+        grouped = _.groupBy(ProjectT, item => `"${item.project.id}+${item.employee.id}"`); // group on 'attribute1' & 'attribute2'
+
+        newTable = _.map(grouped, function(value, key) { // create the new table based on the two attributes
             return{ 
                 IDGroup: key,
                 ProjectID:  _.reduce(value, function(total, o) { 
@@ -128,35 +116,21 @@ export class Aggregation extends React.Component {
                 }, 0) 
             };
             });
-        var resultfin = result3.sort((a, b) => a.ProjectID - b.ProjectID);
 
-        const final = resultfin.map((data, i) => (
+        sorted = newTable.sort((a, b) => a.ProjectID - b.ProjectID); // sort using the first attribute
+
+        return final = sorted.map((data, i) => ( // displays new table values on screen
             <div key={i}>
-               <table>
-                <tbody>
-                <tr>
-                <td>
-                Project
-                <h5>{data.Project}</h5>
-                </td>
-                <td>
-                Employee
-                <h5>{data.Employee}</h5>
-                </td>  
-                <td>
-                Hours
-                <h5>{data.Hours}</h5>
-                </td>
-                </tr>
-                </tbody>
-                </table>
-            </div>
-            ));
-        return final;   
-        }else if(text1 == "employee.name" && text2 == "project.name"){
-            const grouped = _.groupBy(ProjectT, item => `"${item.employee.id}+${item.project.id}"`);
-            // RAGGRUPPAMENTO MULTIPLO PROJECT.ID E EMPLOYEE.ID
-            var result3 = _.map(grouped, function(value, key) {
+               <TableOn_Project_Employee // Use the view created in TableViewer.js
+               project = {data.Project}
+               employee = {data.Employee}
+               hours = {data.Hours}/>
+            </div>));
+           
+        }else if(attribute1 === "employee.name" && attribute2 === "project.name"){ // if attributes have these values
+            grouped = _.groupBy(ProjectT, item => `"${item.employee.id}+${item.project.id}"`); // Same process
+
+            newTable = _.map(grouped, function(value, key) {
                 return{ 
                     IDGroup: key,
                     EmployeeID:  _.reduce(value, function(total, o) { 
@@ -173,37 +147,21 @@ export class Aggregation extends React.Component {
                     }, 0) 
                 };
                 });
-            var resultfin = result3.sort((a, b) => a.EmployeeID - b.EmployeeID);
-    
-            const final = resultfin.map((data, i) => (
+            sorted = newTable.sort((a, b) => a.EmployeeID - b.EmployeeID);
+            return final = sorted.map((data, i) => (
                 <div key={i}>
-                   <table>
-                    <tbody>
-                    <tr>
-                    <td>
-                    Employee
-                    <h5>{data.Employee}</h5>
-                    </td> 
-                    <td>
-                    Project
-                    <h5>{data.Project}</h5>
-                    </td> 
-                    <td>
-                    Hours
-                    <h5>{data.Hours}</h5>
-                    </td>
-                    </tr>
-                    </tbody>
-                    </table>
-                </div>
-                ));
-            return final;   
+                   <TableOn_Employee_Project // Use view created in TableViewer.js
+                   employee = {data.Employee}
+                   project = {data.Project}
+                   hours = {data.Hours}/>
+                </div>));
+              
         }
 
-        if(text1 == "project.name" && text2 == "date"){
-            const grouped = _.groupBy(ProjectT, item => `"${item.project.id}+${item.date}"`);
-            // RAGGRUPPAMENTO MULTIPLO PROJECT.ID E EMPLOYEE.ID
-            var result3 = _.map(grouped, function(value, key) {
+        if(attribute1 === "project.name" && attribute2 === "date"){ //same control process..
+            grouped = _.groupBy(ProjectT, item => `"${item.project.id}+${item.date}"`);
+
+            newTable = _.map(grouped, function(value, key) {
                 return{ 
                     IDGroup: key,
                     ProjectID:  _.reduce(value, function(total, o) { 
@@ -220,35 +178,18 @@ export class Aggregation extends React.Component {
                     }, 0) 
                 };
                 });
-            var resultfin = result3.sort((a, b) => a.ProjectID - b.ProjectID);
-    
-            const final = resultfin.map((data, i) => (
+            sorted = newTable.sort((a, b) => a.ProjectID - b.ProjectID);
+            return final = sorted.map((data, i) => (
                 <div key={i}>
-                   <table>
-                    <tbody>
-                    <tr>
-                    <td>
-                    Project
-                    <h5>{data.Project}</h5>
-                    </td>
-                    <td>
-                    Date
-                    <h5>{data.Date}</h5>
-                    </td>  
-                    <td>
-                    Hours
-                    <h5>{data.Hours}</h5>
-                    </td>
-                    </tr>
-                    </tbody>
-                    </table>
-                </div>
-                ));
-            return final;   
-        }else if(text1 == "date" && text2 == "project.name"){
-                const grouped = _.groupBy(ProjectT, item => `"${item.date}+${item.project.id}"`);
-                // RAGGRUPPAMENTO MULTIPLO PROJECT.ID E EMPLOYEE.ID
-                var result3 = _.map(grouped, function(value, key) {
+                   <TableOn_Project_Date
+                   project = {data.Project}
+                   date = {data.Date}
+                   hours = {data.Hours}/>
+                </div>));
+        }else if(attribute1 === "date" && attribute2 === "project.name"){
+                grouped = _.groupBy(ProjectT, item => `"${item.date}+${item.project.id}"`);
+
+                newTable = _.map(grouped, function(value, key) {
                     return{ 
                         IDGroup: key,
                         ProjectID:  _.reduce(value, function(total, o) { 
@@ -262,40 +203,24 @@ export class Aggregation extends React.Component {
                         }, 0),
                         Hours: _.reduce(value, function(total, o) { 
                             return total + o.hours;
-                        }, 0) 
-                    };
-                    });
-                var resultfin = result3.sort((a, b) => a.Date - b.Date);
-        
-                const final = resultfin.map((data, i) => (
+                        }, 0)};
+                    }
+                );
+                
+                sorted = newTable.sort((a, b) => a.Date - b.Date);
+                return final = sorted.map((data, i) => (
                     <div key={i}>
-                       <table>
-                        <tbody>
-                        <tr>
-                        <td>
-                        Date
-                        <h5>{data.Date}</h5>
-                        </td> 
-                        <td>
-                        Project
-                        <h5>{data.Project}</h5>
-                        </td> 
-                        <td>
-                        Hours
-                        <h5>{data.Hours}</h5>
-                        </td>
-                        </tr>
-                        </tbody>
-                        </table>
-                    </div>
-                    ));
-                return final;   
+                       <TableOn_Date_Project
+                       date = { data.Date}
+                       project = {data.Project}
+                       hours = {data.Hours}/></div>));
         }
 
-        if(text1 == "date" && text2 == "employee.name"){
-            const grouped = _.groupBy(ProjectT, item => `"${item.date}+${item.employee.name}"`);
-            // RAGGRUPPAMENTO MULTIPLO PROJECT.ID E EMPLOYEE.ID
-            var result3 = _.map(grouped, function(value, key) {
+        if(attribute1 === "date" && attribute2 === "employee.name"){
+            
+            grouped = _.groupBy(ProjectT, item => `"${item.date}+${item.employee.name}"`);
+
+            newTable = _.map(grouped, function(value, key) {
                 return{ 
                     IDGroup: key,
                     ProjectID:  _.reduce(value, function(total, o) { 
@@ -312,35 +237,17 @@ export class Aggregation extends React.Component {
                     }, 0) 
                 };
                 });
-            var resultfin = result3.sort((a, b) => a.Date - b.Date);
-    
-            const final = resultfin.map((data, i) => (
+            sorted = newTable.sort((a, b) => a.Date - b.Date);
+            return final = sorted.map((data, i) => (
                 <div key={i}>
-                   <table>
-                    <tbody>
-                    <tr>
-                    <td>
-                    Date
-                    <h5>{data.Date}</h5>
-                    </td>  
-                    <td>
-                    Employee
-                    <h5>{data.Employee}</h5>
-                    </td>
-                    <td>
-                    Hours
-                    <h5>{data.Hours}</h5>
-                    </td>
-                    </tr>
-                    </tbody>
-                    </table>
-                </div>
-                ));
-            return final;   
-        }else if(text1 == "employee.name" && text2 == "date"){
-                const grouped = _.groupBy(ProjectT, item => `"${item.employee.name}+${item.date}"`);
-                // RAGGRUPPAMENTO MULTIPLO PROJECT.ID E EMPLOYEE.ID
-                var result3 = _.map(grouped, function(value, key) {
+                   <TableOn_Date_Employee
+                   date={data.Date}
+                   employee={data.Employee}
+                   hours={data.Hours}/></div>));
+        }else if(attribute1 === "employee.name" && attribute2 === "date"){
+                grouped = _.groupBy(ProjectT, item => `"${item.employee.name}+${item.date}"`);
+
+                newTable = _.map(grouped, function(value, key) {
                     return{ 
                         IDGroup: key,
                         ProjectID:  _.reduce(value, function(total, o) { 
@@ -356,52 +263,38 @@ export class Aggregation extends React.Component {
                             return total + o.hours;
                         }, 0) 
                     };
-                    });
-                var resultfin = result3.sort((a, b) => a.Employee - b.Employee);
-        
-                const final = resultfin.map((data, i) => (
+                });
+                sorted = newTable.sort((a, b) => a.Employee - b.Employee);
+                return final = sorted.map((data, i) => (
                     <div key={i}>
-                       <table>
-                        <tbody>
-                        <tr>
-                        <td>
-                        Employee
-                        <h5>{data.Employee}</h5>
-                        </td> 
-                        <td>
-                        Date
-                        <h5>{data.Date}</h5>
-                        </td> 
-                        <td>
-                        Hours
-                        <h5>{data.Hours}</h5>
-                        </td>
-                        </tr>
-                        </tbody>
-                        </table>
-                    </div>
-                    ));
-                return final;   
+                       <TableOn_Employee_Date
+                       employee={data.Employee}
+                       date={data.Date}
+                       hours={data.Hours}/>
+                    </div>));
         }
     }
 
+    /* See on console the actual values of this class, related to: first and second combobox*/
     publish() {
-        console.log( this.state.comboboxValue1, this.state.comboboxValue2, this.state.aggregationValue);
+        console.log('1st value:',this.state.comboboxValue1,'    ','2nd value:', this.state.comboboxValue2);
     }
   
-    handleClick = () => {
-        this.setState(prevState => {
-        console.log(prevState);
-        return { isClicked: !prevState.isClicked };
-        });
-    };
-
     handleChange({ target }) {
         this.setState({
         [target.name]: target.value
         });
     }
 
+    /*
+    handleClick = () => {
+        this.setState(prevState => {
+        console.log(prevState);
+        return { isClicked: !prevState.isClicked };
+        });
+    };*/
+
+  /**Html code & function execution on page */
   render() {
     return (
     <div>
@@ -417,9 +310,7 @@ export class Aggregation extends React.Component {
               <option value="employee.name">Employee</option>
               <option value="date">Date</option>
     </select>
-        <div>
-            {this.Aggregation(this.state.comboboxValue1, this.state.comboboxValue2)}          
-        </div>
+    <div> {this.mainAggregation(this.state.comboboxValue1, this.state.comboboxValue2)} </div>
     </div>
     );
   }
--- client/src/components/TableViewer.js
+++ client/src/components/TableViewer.js
@@ -1,4 +1,5 @@
-export const ProjectV = ({ project, employee, date, hours }) => {
+
+export const ProjectV = ({ project, employee, date, hours }) => { // first view of the page, shows the original table
     if (!project) return <div />;
     return (
       <table>
@@ -6,22 +7,230 @@ export const ProjectV = ({ project, employee, date, hours }) => {
           <tr>
             <td>
             Name
-              <h5>{project}</h5>
+            <h5>{project}</h5>
             </td>
             <td>
             Employee
-              <h5>{employee}</h5>
+            <h5>{employee}</h5>
             </td>
             <td>
             Date
-              <h4>{date}</h4>
+            <h4>{date}</h4>
             </td>
             <td>
             Hours
-              <p>{hours}</p>
+            <p>{hours}</p>
             </td>
           </tr>
         </tbody>
       </table>
     );
   };
+
+  export const TableOnProject = ({ project, hours }) => {
+    if (!project) return <div />;
+    return (
+      <table>
+      <tbody>
+      <tr>
+      <td>
+      Project
+      <h5>{project}</h5>
+      </td> 
+      <td>
+      Hours
+      <h5>{hours}</h5>
+      </td>
+      </tr>
+      </tbody>
+      </table>
+    );
+  };
+
+  export const TableOnEmployee = ({ employee, hours }) => {
+    if (!employee) return <div />;
+    return (
+      <table>
+      <tbody>
+      <tr>
+      <td>
+      Employee
+      <h5>{employee}</h5>
+      </td> 
+      <td>
+      Hours
+      <h5>{hours}</h5>
+      </td>
+      </tr>
+      </tbody>
+      </table>
+    );
+  };
+
+  export const TableOnDate = ({ date, hours }) => {
+    if (!date) return <div />;
+    return (
+      <table>
+      <tbody>
+      <tr>
+      <td>
+      Date
+      <h5>{date}</h5>
+      </td> 
+      <td>
+      Hours
+      <h5>{hours}</h5>
+      </td>
+      </tr>
+      </tbody>
+      </table>
+    );
+  };
+
+  export const TableOn_Project_Employee = ({ project, employee, hours }) => {
+    if (!project) return <div />;
+    return (
+      <table>
+      <tbody>
+      <tr>
+      <td>
+      Project
+      <h5>{project}</h5>
+      </td> 
+      <td>
+      Employee
+      <h5>{employee}</h5>
+      </td>
+      <td>
+      Hours
+      <h5>{hours}</h5>
+      </td>
+      </tr>
+      </tbody>
+      </table>
+    );
+  };
+
+  export const TableOn_Employee_Project = ({ employee, project, hours }) => {
+    if (!employee) return <div />;
+    return (
+      <table>
+      <tbody>
+      <tr>
+        <td>
+        Employee
+        <h5>{employee}</h5>
+        </td>
+        <td>
+        Project
+        <h5>{project}</h5>
+        </td> 
+        <td>
+        Hours
+        <h5>{hours}</h5>
+        </td>
+        </tr>
+      </tbody>
+      </table>
+    );
+  };
+
+  export const TableOn_Project_Date = ({ project, date, hours }) => {
+    if (!project) return <div />;
+    return (
+      <table>
+      <tbody>
+      <tr>
+        <td>
+        Project
+        <h5>{project}</h5>
+        </td>
+        <td>
+        Date
+        <h5>{date}</h5>
+        </td> 
+        <td>
+        Hours
+        <h5>{hours}</h5>
+        </td>
+        </tr>
+      </tbody>
+      </table>
+    );
+  };
+
+  export const TableOn_Date_Project = ({ date, project, hours }) => {
+    if (!date) return <div />;
+    return (
+      <table>
+      <tbody>
+      <tr>
+        <td>
+        Date
+        <h5>{date}</h5>
+        </td> 
+        <td>
+        Project
+        <h5>{project}</h5>
+        </td>
+        <td>
+        Hours
+        <h5>{hours}</h5>
+        </td>
+        </tr>
+      </tbody>
+      </table>
+    );
+  };
+
+  export const TableOn_Date_Employee = ({ date, employee, hours }) => {
+    if (!date) return <div />;
+    return (
+      <table>
+      <tbody>
+      <tr>
+        <td>
+        Date
+        <h5>{date}</h5>
+        </td> 
+        <td>
+        Employee
+        <h5>{employee}</h5>
+        </td>
+        <td>
+        Hours
+        <h5>{hours}</h5>
+        </td>
+        </tr>
+      </tbody>
+      </table>
+    );
+  };
+
+  export const TableOn_Employee_Date = ({ employee, date, hours }) => {
+    if (!employee) return <div />;
+    return (
+      <table>
+      <tbody>
+      <tr>
+        <td>
+        Employee
+        <h5>{employee}</h5>
+        </td>
+        <td>
+        Date
+        <h5>{date}</h5>
+        </td> 
+        <td>
+        Hours
+        <h5>{hours}</h5>
+        </td>
+        </tr>
+      </tbody>
+      </table>
+    );
+  };
+
+
+  
+  
